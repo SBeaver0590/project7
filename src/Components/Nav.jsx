b56a629b21ff37f3410c387761218b512a3edd6d
Simone Beaver <simone.beaver@verizon.com>
2019-08-07 12:36:23 -0500

    add config.js

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const Nav = props => {
+    return (
+        <div>
+         <nav class="main-nav">
+            <ul>
+                <li><a href='https://google.com'>Cats</a></li>
+                <li><a href='https://google.com'>Dogs</a></li>
+                <li><a href='https://google.com'>Computers</a></li>
+            </ul>
+         </nav>   
+        </div>
+    );
+};
+
+// Nav.propTypes = {
+    
+// };
+
+export default Nav;
